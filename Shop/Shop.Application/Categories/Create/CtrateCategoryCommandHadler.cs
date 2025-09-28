8c3ab962ed25290273c26bf8f0dbacfc80eacb6a
Matin Tavakoli <matin20051384@gmail.com>
2025-09-29 00:30:49 +0330

    Category Use Case

---
+++ Shop/Shop.Application/Categories/Create/CtrateCategoryCommandHadler.cs
@@ -0,0 +1,25 @@
+﻿using Common.Application;
+using Shop.Domain.Category.Entities;
+using Shop.Domain.Category.Repositories;
+using Shop.Domain.Category.Services;
+
+namespace Shop.Application.Categories.Create;
+
+public class CtrateCategoryCommandHadler : IBaseCommandHandler<CreateCategoryCommand>
+{
+    private readonly ICategoryRepository _repository;
+    private readonly ICategoryDomainService _domainService;
+
+    public CtrateCategoryCommandHadler(ICategoryRepository repository, ICategoryDomainService domainService)
+    {
+        _repository = repository;
+        _domainService = domainService;
+    }
+    public async Task<OperationResult> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var category = new Category(request.Title, request.Slug, request.SeoData, _domainService);
+        _repository.Add(category);
+        await _repository.Save();
+        return OperationResult.Success();
+    }
+}
---
+++ Shop/Shop.Application/Categories/Edit/EditCategoryCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Common.Application;
+using Shop.Domain.Category;
+using Shop.Domain.Category.Repositories;
+using Shop.Domain.Category.Services;
+
+namespace Shop.Application.Categories.Edit
+{
+    public class EditCategoryCommandHandler : IBaseCommandHandler<EditCategoryCommand>
+    {
+        private readonly ICategoryRepository _repository;
+        private readonly ICategoryDomainService _domainServicer;
+
+        public EditCategoryCommandHandler(ICategoryRepository repository, ICategoryDomainService domainServicer)
+        {
+            _repository = repository;
+            _domainServicer = domainServicer;
+        }
+
+        public async Task<OperationResult> Handle(EditCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var category = await _repository.GetTracking(request.Id);
+            if (category == null)
+                return OperationResult.NotFound();
+
+            category.Edit(request.Title, request.Slug, request.SeoData, _domainServicer);
+            await _repository.Save();
+            return OperationResult.Success();
+        }
+    }
+}
